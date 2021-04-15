98d4142129a82b4d02e2d65b33bf3e6a9d32d96f
Vistillor <Victo1577@gmail.com>
2021-04-15 12:06:04 +0200

    disparo de torres siguiendo al enemigo y movimiento del enemigo siguiendo el camino

---
+++ Assets/Script/EnemyMovi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovi : MonoBehaviour
+{
+
+    public NavMeshAgent agente;
+    public Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente.SetDestination(position);
+    }
+
+   
+}
---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Tooltip("Referencia al prefab de la bala")]
+    public GameObject bulletPrefab;
+
+    [Tooltip("Referencia al enemigo al que voy a disparar")]
+    public GameObject enemy;
+
+    public float height;
+
+    void Update()
+    {
+        // Instancia una bala cada vez que el jugador pulsa el espacio
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
+            bala.GetComponent<Movimiento>().enemy = enemy;
+        }
+    }
+}
