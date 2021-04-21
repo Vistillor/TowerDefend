b2313407c8df18564c2ef644e6e7374e4516f25f
Vistillor <Victo1577@gmail.com>
2021-04-21 18:56:26 +0200

    triggerTower

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -12,17 +12,40 @@ public class Tower : MonoBehaviour
 
     public float height;
 
+    public float ratio;
+
+    private void Start()
+    {
+        InvokeRepeating("Shoot", 3, ratio);
+    }
+
     void Update()
     {
         // Instancia una bala cada vez que el jugador pulsa el espacio
-        if (Input.GetKeyDown(KeyCode.Space))
+      //  if (Input.GetKeyDown(KeyCode.Space))
         {
             //comprueba si el enemigo existe o ya ha sido destruido
-            if (enemy != null)
-            {
-                GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
-                bala.GetComponent<Movimiento>().enemy = enemy;
-            }
+           
         }
+
+       
+    }
+    private void Shoot()
+    {
+        if (enemy != null)
+        {
+            GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
+            bala.GetComponent<Movimiento>().enemy = enemy;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //combrueba si es el enemigo el que entra en el collaider
+        if (other.CompareTag("enemigo"))
+        {
+            enemy = other.gameObject;
+        }
+
+        
     }
 }
